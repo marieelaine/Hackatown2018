4a47ec4adc976ef9920c203d9d75dd0966329574
Sebastien Cadorette <cado.94@me.com>
2018-01-20 23:11:57 -0500

    SC : Suppression du service Users.

--- hackatown/src/app/app.component.ts
+++ hackatown/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
 })
+
 export class AppComponent {
-  title: string = 'My first AGM project';
+  user_data: any;
+  constructor() { }
+
+  ngOnInit() {
+    this.user_data = require('./data_user.json');
+    console.log(this.user_data);
+  }
 }
