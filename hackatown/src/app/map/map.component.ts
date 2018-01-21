af1b70c665b22e88868e333f8684eda998edccab
Sebastien Cadorette <cado.94@me.com>
2018-01-21 04:13:08 -0500

    SC : Avancement de la map.

--- hackatown/src/app/map/map.component.ts
+++ hackatown/src/app/map/map.component.ts
@@ -14,7 +14,6 @@ declare var google: any;
 
 export class MapComponent implements OnInit {
   users: Users;
-  u:User[];
   centerLongitude = -73.56725599999999;
   centerLatitude = 45.5016889;
 
@@ -32,38 +31,11 @@ export class MapComponent implements OnInit {
   }
 
   initMap(): void {
-    var locations = [
-      {lat: -31.563910, lng: 147.154312},
-      {lat: -33.718234, lng: 150.363181},
-      {lat: -33.727111, lng: 150.371124},
-      {lat: -33.848588, lng: 151.209834},
-      {lat: -33.851702, lng: 151.216968},
-      {lat: -34.671264, lng: 150.863657},
-      {lat: -35.304724, lng: 148.662905},
-      {lat: -36.817685, lng: 175.699196},
-      {lat: -36.828611, lng: 175.790222},
-      {lat: -37.750000, lng: 145.116667},
-      {lat: -37.759859, lng: 145.128708},
-      {lat: -37.765015, lng: 145.133858},
-      {lat: -37.770104, lng: 145.143299},
-      {lat: -37.773700, lng: 145.145187},
-      {lat: -37.774785, lng: 145.137978},
-      {lat: -37.819616, lng: 144.968119},
-      {lat: -38.330766, lng: 144.695692},
-      {lat: -39.927193, lng: 175.053218},
-      {lat: -41.330162, lng: 174.865694},
-      {lat: -42.734358, lng: 147.439506},
-      {lat: -42.734358, lng: 147.501315},
-      {lat: -42.735258, lng: 147.438000},
-      {lat: -43.999792, lng: 170.463352}
-    ]
-
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 3,
       center: {lat: this.centerLatitude, lng: this.centerLongitude}
     });
 
-    
     for(var i = 0; i < 1 ; i++) {
       var marker = new google.maps.Marker({
         position: this.users.getUsers()[i].localisation,
@@ -71,7 +43,7 @@ export class MapComponent implements OnInit {
       });
 
       marker.addListener('click', function(e) {
-        map.setZoom(11);
+        map.setZoom(3);
         map.setCenter(marker.getPosition());
       })
 
